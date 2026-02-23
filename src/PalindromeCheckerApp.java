77bfc124b11b018bd89659085e85b0f66701fdc4
Beni-004 <cuddlesbeniwal@gmail.com>
2026-02-23 10:43:52 +0530

    Palindrome checker using linked list

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,64 @@
-import java.util.*;
-
 public class PalindromeCheckerApp {
+
+    // Node class for singly linked list
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node createLinkedList(String str) {
+        if (str == null || str.length() == 0)
+            return null;
+        Node head = new Node(str.charAt(0));
+        Node current = head;
+        for (int i = 1; i < str.length(); i++) {
+            current.next = new Node(str.charAt(i));
+            current = current.next;
+        }
+        return head;
+    }
+    public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null)
+            return true;
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+        Node temp = secondHalf;
+        while (temp != null) {
+            if (firstHalf.data != temp.data)
+                return false;
+            firstHalf = firstHalf.next;
+            temp = temp.next;
+        }
+        return true;
+    }
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
     public static void main(String[] args) {
-        String input = "refer";
-        Deque<Character> deque = new ArrayDeque<>();
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
+        String input = "madam";
+        Node head = createLinkedList(input);
+        if (isPalindrome(head)) {
+            System.out.println(input+" is a Palindrome");
+        } else {
+            System.out.println(input+"is not Palindrome");
         }
-        boolean isPalindrome = true;
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
